0bfbf2b3302e7bf616eed178fc023d7d05b5a2d3
Dina Riad <dinayriad@gmail.com>
2019-07-18 18:55:09 -0700

    Containter_Semi_Design

---
+++ app/src/main/java/com/bignerdranch/android/haya/view/activities/ContainerActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.haya.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bignerdranch.android.haya.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
